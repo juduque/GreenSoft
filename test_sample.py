127f72a16c413853942d2cc48f04dd5961d8b23e
Juan <juanduque110024@hotmail.com>
2022-08-20 15:47:47 -0500

    SonarCloud Configure5

--- test_sample.py
+++ test_sample.py
@@ -4,13 +4,17 @@ from main import app
 
 client = TestClient(app)
 
-def test_index_route():
-    response = client.get('/items/1')
-    assert response.status_code == 200
-    assert response.json() == {'item_id': 1, 'q': None}
+# def test_index_route():
+#     response = client.get('/items/1')
+#     assert response.status_code == 200
+#     assert response.json() == {'item_id': 1, 'q': None}
+
+# def test_index_route():
+#     response = client.get('/items/hola')
+#     print(response.json())
+#     assert response.status_code == 200
+#     assert response.json() == {'item_id': 1, 'q': None}
 
 def test_index_route():
-    response = client.get('/items/hola')
-    print(response.json())
-    assert response.status_code == 200
-    assert response.json() == {'item_id': 1, 'q': None}
+    response = client.get('/items/1')
+    assert response.status_code == 404
