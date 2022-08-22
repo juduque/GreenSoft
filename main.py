39ca51898a5b49f0aedf8d6dd537dc2262546302
Juan <juanduque110024@hotmail.com>
2022-08-21 22:15:21 -0500

    Add metrics App

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import requests
-from fastapi import FastAPI, Response, status
-
-from pydantic import BaseModel # adicionar en los imports en el main.py
-
-app = FastAPI()
-url = 'https://62fd7b30b9e38585cd52637e.mockapi.io/udem/taller2/'
-
-#from .auth_users import handler as auth
+import random
+from pydantic import BaseModel  # adicionar en los imports en el main.py
 from info_users import handler as info
 from auth_users import handler as auth
 from role_users import handler as role
+from fastapi import FastAPI, Response, status
+from prometheus_client import start_http_server
+from prometheus_metrics import process_request, counter, latency
 
+# Print metrics y/n
+METRICS = True
+
+# Star App
+app = FastAPI()
+url = 'https://62fd7b30b9e38585cd52637e.mockapi.io/udem/taller2/'
+
+
+# Start metrics
+process_request(random.random())
+counter()
+latency()
+
+
+# from .auth_users import handler as auth
 app.include_router(info.router)
 app.include_router(auth.router)
 app.include_router(role.router)
+
+
+# Star localhost metrics
+if METRICS:
+    start_http_server(3000)
--- prometheus_metrics.py
+++ prometheus_metrics.py
@@ -1,9 +1,8 @@
-from prometheus_client import start_http_server, Summary
-import random
+import prometheus_client
 import time
 
 # Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+REQUEST_TIME = prometheus_client.Summary('request_processing_seconds', 'Time spent processing request')
 
 
 # Decorate function with metric.
@@ -13,9 +12,19 @@ def process_request(t):
     time.sleep(t)
 
 
-if __name__ == '__main__':
-    # Start up the server to expose the metrics.
-    start_http_server(8000)
-    # Generate some requests.
-    while True:
-        process_request(random.random())
+def counter():
+    c = prometheus_client.Counter('my_failures', 'Description of counter')
+    c.inc()     # Increment by 1
+    c.inc(1.6)  # Increment by given value
+
+
+def latency():
+    s = prometheus_client.Summary('request_latency_seconds', 'Description of summary')
+    s.observe(4.7)    # Observe 4.7 (seconds in this case)
+
+# if __name__ == '__main__':
+#     # Start up the server to expose the metrics.
+#     start_http_server(8000)
+#     # Generate some requests.
+#     while True:
+#         process_request(random.random())
